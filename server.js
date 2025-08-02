6417a7850429d9e9185e66a60c0bcf9e26c5e1ae
Bimbika-Ganapathy <bimbikag@gmail.com>
2025-08-03 00:31:52 +0530

    Initial tracker server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Tracker is running');
+});
+ 
+app.get('/track.png', (req, res) => {
+  const log = `${new Date().toISOString()} - ${req.ip} - ${req.headers['user-agent']}\n`;
+  fs.appendFileSync('views.log', log);
+  const pixel = Buffer.from(
+    'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==',
+    'base64'
+  );
+  res.set('Content-Type', 'image/png');
+  res.send(pixel);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
